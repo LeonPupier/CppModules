d59d7e4906699385be837d8cf3bb99f7e8802214
LeonPupier <leonpupier@gmail.com>
2023-09-26 14:38:00 +0200

    Cpp09/ex01 RPN calculator finished

---
+++ Cpp09/ex01/RPN.Class/RPN.cpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpupier <lpupier@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/17 08:27:14 by lpupier           #+#    #+#             */
+/*   Updated: 2023/09/26 14:37:21 by lpupier          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "RPN.hpp"
+
+bool	processRPN(std::string operation)
+{
+	std::stack<int>	stack;
+	int				a;
+	int				b;
+
+	for (size_t i = 0; i < operation.length(); i++)
+	{
+		if (operation[i] == ' ')
+			continue ;
+		else if (operation[i] == '+' || operation[i] == '-' || operation[i] == '/' || operation[i] == '*')
+		{
+			if (stack.size() < 2)
+			{
+				std::cout << RED << "[Error] " << RESET << "Not enough numbers to perform the operation." << std::endl;
+				return (false);
+			}
+
+			a = stack.top();
+			stack.pop();
+			b = stack.top();
+			stack.pop();
+			
+			if (operation[i] == '+')
+				stack.push(b + a);
+			else if (operation[i] == '-')
+				stack.push(b - a);
+			else if (operation[i] == '/')
+				stack.push(b / a);
+			else if (operation[i] == '*')
+				stack.push(b * a);
+		}
+		else if (operation[i] >= '0' && operation[i] <= '9')
+			stack.push(operation[i] - 48);
+		else
+		{
+			std::cout << RED << "[Error] " << RESET << "Invalid character: '" << operation[i] << "'." << std::endl;
+			return (false);
+		}
+	}
+
+	std::cout << GREEN << "[Result] " << RESET << stack.top() << std::endl;
+	return (true);
+}
---
+++ Cpp09/ex01/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpupier <lpupier@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 13:46:15 by lpupier           #+#    #+#             */
+/*   Updated: 2023/09/26 14:36:16 by lpupier          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "RPN.Class/RPN.hpp"
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		std::cout << RED << "[Error]" << RESET << " Please provide your operation: '<number> <number> <expression>...' (ex: 4 2 +)." << std::endl;
+		return (1);
+	}
+	processRPN(argv[1]);
+	return (0);
+}
